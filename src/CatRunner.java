1c94b1c0804b0f53b68b1933f566ab87d1bc2598
Raida Hasan <raidahasan09@gmail.com>
2023-09-24 23:11:14 -0400

    Introduce change

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,14 @@
 public class CatRunner {
+    public static void main (String[] args){
+
+        Cat cat1 = new Cat ("Ava", 5, 10);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat ("Stephanie", 7, 9);
+        cat2.printCatInfo();
+        cat2.introduce();
+    }
+
+
 }
